421bd41e27402d1ae777666dbf35a9fddb83bbc4
Dan Phillips <dan@danphillips.ca>
2023-12-03 16:59:17 -0400

    Major change for tracking selected questions.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
+// Create an object to keep track of selected questions
+const selectedQuestions = {};
+
 document.addEventListener('DOMContentLoaded', function () {
   fetch('jeopardy_data.json')
     .then(response => {
@@ -27,9 +30,20 @@ function displayJeopardyBoard(categories) {
       questionDiv.className = 'jeopardy-question';
       questionDiv.textContent = `$${question.value}`;
 
-      questionDiv.addEventListener('click', function () {
+      if (selectedQuestions[question.value]) {
+        questionDiv.classList.add('question-selected');
+        questionDiv.removeEventListener('click', handleClick);
+      } else {
+        questionDiv.addEventListener('click', handleClick);
+      }
+
+      function handleClick() {
+        selectedQuestions[question.value] = true;
+        questionDiv.classList.add('question-selected');
+
         showQuestionModal(question.question, question.answer);
-      });
+        questionDiv.removeEventListener('click', handleClick);
+      }
 
       categoryDiv.appendChild(questionDiv);
     });
@@ -41,7 +55,7 @@ function displayJeopardyBoard(categories) {
 let questionModal = null; // Variable to store the current question modal
 
 function showQuestionModal(question, answer) {
-  closeModal(); // Close any existing modal before displaying a new one
+  closeModal();
 
   const modal = document.createElement('div');
   modal.className = 'modal';
@@ -52,19 +66,19 @@ function showQuestionModal(question, answer) {
     </div>
   `;
   document.body.appendChild(modal);
-  document.body.style.overflow = 'hidden'; // Disable scrolling when modal is open
+  document.body.style.overflow = 'hidden';
 
-  questionModal = modal; // Store the reference to the current question modal
+  questionModal = modal;
 
   modal.addEventListener('click', function () {
-    showAnswerModal(answer); // Pass the answer to showAnswerModal
+    showAnswerModal(answer);
   });
 }
 
 function showAnswerModal(answer) {
   if (questionModal !== null) {
-    questionModal.remove(); // Close the question modal
-    questionModal = null; // Reset the reference to the question modal
+    questionModal.remove();
+    questionModal = null;
   }
 
   const modal = document.createElement('div');
@@ -76,7 +90,7 @@ function showAnswerModal(answer) {
     </div>
   `;
   document.body.appendChild(modal);
-  document.body.style.overflow = 'hidden'; // Disable scrolling when modal is open
+  document.body.style.overflow = 'hidden';
 
   modal.addEventListener('click', function () {
     closeModal();
@@ -87,10 +101,10 @@ function closeModal() {
   const modal = document.querySelector('.modal');
   if (modal) {
     modal.remove();
-    document.body.style.overflow = ''; // Enable scrolling when modal is closed
+    document.body.style.overflow = '';
     if (questionModal !== null) {
-      questionModal.remove(); // Close the question modal if it exists
-      questionModal = null; // Reset the reference to the question modal
+      questionModal.remove();
+      questionModal = null;
     }
   }
 }
