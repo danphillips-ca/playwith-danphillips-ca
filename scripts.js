8e7e7e1400705bdc4d7e43dcd6dbfc45f90481f8
Dan Phillips <dan@danphillips.ca>
2023-12-03 00:12:17 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
   fetch('jeopardy_data.json')
-    .then(response => response.json())
-    .then(data => displayJeopardyBoard(data.categories));
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => displayJeopardyBoard(data.categories))
+    .catch(error => {
+      console.error('There was a problem fetching the data:', error);
+      const jeopardyBoard = document.getElementById('jeopardyBoard');
+      jeopardyBoard.innerHTML = 'Failed to load Jeopardy game data. Please try again later.';
+    });
 });
 
 function displayJeopardyBoard(categories) {
@@ -18,7 +28,7 @@ function displayJeopardyBoard(categories) {
       questionDiv.textContent = `$${question.value}`;
 
       questionDiv.addEventListener('click', function () {
-        alert(question.question); // Show the question on click (replace with your logic)
+        showQuestionModal(question.question);
       });
 
       categoryDiv.appendChild(questionDiv);
@@ -26,4 +36,26 @@ function displayJeopardyBoard(categories) {
 
     jeopardyBoard.appendChild(categoryDiv);
   });
+}
+
+function showQuestionModal(question) {
+  const modal = document.createElement('div');
+  modal.className = 'modal';
+  modal.innerHTML = `
+    <div class="modal-content">
+      <span class="close" onclick="closeModal()">&times;</span>
+      <h2>Question</h2>
+      <p>${question}</p>
+    </div>
+  `;
+  document.body.appendChild(modal);
+  document.body.style.overflow = 'hidden'; // Disable scrolling when modal is open
+}
+
+function closeModal() {
+  const modal = document.querySelector('.modal');
+  if (modal) {
+    modal.remove();
+    document.body.style.overflow = ''; // Enable scrolling when modal is closed
+  }
 }
