633ee61371a2ab2edc9bb773c977f91be1416193
Dan Phillips <dan@danphillips.ca>
2023-12-04 19:24:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const categoryTitles = document.querySelector('.category-titles');
-  const questionValues = document.querySelector('.question-values');
+  const categoryTitles = document.querySelector('.categories');
+  const questionValues = document.querySelector('.questions');
   const modal = document.querySelector('.modal-content');
   const modalTitle = document.querySelector('.modal-title');
   const questionText = document.querySelector('.question-text');
@@ -11,9 +11,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
   let data; // Variable to hold the loaded JSON data
 
-  // Fetch JSON data
   fetch('gamedata/gamedata.json')
-    .then(response => response.json())
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
     .then(jsonData => {
       data = jsonData;
 
@@ -28,7 +32,7 @@ document.addEventListener('DOMContentLoaded', function() {
       });
 
       // Handle click events on questions
-      const questions = document.querySelectorAll('.question-values div');
+      const questions = document.querySelectorAll('.questions div');
       questions.forEach(question => {
         question.addEventListener('click', () => {
           const value = question.getAttribute('data-value');
@@ -48,7 +52,10 @@ document.addEventListener('DOMContentLoaded', function() {
         });
       });
     })
-    .catch(error => console.error('Error fetching data:', error));
+    .catch(error => {
+      console.error('Error fetching or processing data:', error);
+      // You can add specific error handling or display an error message to the user here
+    });
 
   // Handle modal close button
   modalClose.addEventListener('click', () => {
